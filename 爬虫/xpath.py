e1e165462d4ea14cd4a4e7217a6cd54ba5c43c2b
WilliamL71Oi <37209194+WilliamL71Oi@users.noreply.github.com>
2021-07-17 12:06:47 +0800

    Update xpath.py

--- 爬虫/xpath.py
+++ 爬虫/xpath.py
@@ -26,3 +26,31 @@ print('第四个属性值：', li_four)
 第一个属性值:  ['Java API文档']
 第四个属性值： ['配置JDK']
 
+
+# xpath提供了一些节点轴的匹配方法，例如，获取祖节点、子孙节点、兄弟节点等。
+from lxml import etree
+
+html_str = '''
+<div class="video_scroll">
+<li> <a href="javascript:" onclick="login(0)" title="Java API文档">Java API文档</a> </li>
+<li> <a href="javascript:" onclick="login(0)" title="JDK的下载">JDK的下载</a> </li>
+<li> <a href="Javascript:" onclick="login(0)" title="JDK的安装">JDK的安装</a> </li>
+<li> <a href="javascript:" onclick="login(0)" title="配置JDK">配置JDK</a> </li>
+</div>
+'''
+html = etree.HTML(html_str)
+li_last = html.xpath('//div/li[last()]/a/@title')
+print('最后一个属性值：', li_last)
+li = html.xpath('//div/li[position()=1]/a/@title')
+print('第一个位置的属性值：', li)
+li = html.xpath('//div/li[last()-1]/a/@title')
+print('倒数第二个位置的属性值：', li)
+li = html.xpath('//div/li[position()>1]/a/@title')
+print('位置大于1的属性值：', li)
+
+结果：
+最后一个属性值： ['配置JDK']
+第一个位置的属性值： ['Java API文档']
+倒数第二个位置的属性值： ['JDK的安装']
+位置大于1的属性值： ['JDK的下载', 'JDK的安装', '配置JDK']
+
