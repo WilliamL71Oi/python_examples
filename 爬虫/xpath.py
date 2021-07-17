924e71bb4e0c1eb21baa754fa8a84f82aef36ecd
WilliamL71Oi <37209194+WilliamL71Oi@users.noreply.github.com>
2021-07-17 11:59:32 +0800

    Create xpath.py

---
+++ 爬虫/xpath.py
@@ -0,0 +1,28 @@
+
+
+
+
+# 如果同时匹配了多个节点，但只需要其中的某一节点时，可以使用指定索引的方式获取对应的节点内容，不过xpath中的索引是从1开始的。
+from lxml import etree
+
+html_str = '''
+<div class="video_scroll">
+<li> <a href="javascript:" onclick="login(0)" title="Java API文档">Java API文档</a> </li>
+<li> <a href="javascript:" onclick="login(0)" title="JDK的下载">JDK的下载</a> </li>
+<li> <a href="Javascript:" onclick="login(0)" title="JDK的安装">JDK的安装</a> </li>
+<li> <a href="javascript:" onclick="login(0)" title="配置JDK">配置JDK</a> </li>
+</div>
+'''
+html = etree.HTML(html_str)
+li_all = html.xpath('//div/li/a/@title')
+print("所有属性值: ", li_all)
+li_first = html.xpath('//div/li[1]/a/@title')
+print("第一个属性值: ", li_first)
+li_four = html.xpath('//div/li[4]/a/@title')
+print('第四个属性值：', li_four)
+
+结果：
+所有属性值:  ['Java API文档', 'JDK的下载', 'JDK的安装', '配置JDK']
+第一个属性值:  ['Java API文档']
+第四个属性值： ['配置JDK']
+
