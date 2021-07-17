ab644a9e47e79d15a33c746fba1161d160a62582
WilliamL71Oi <37209194+WilliamL71Oi@users.noreply.github.com>
2021-07-17 13:39:43 +0800

    Update xpath.py

--- 爬虫/xpath.py
+++ 爬虫/xpath.py
@@ -64,3 +64,44 @@ print('位置大于1的属性值：', li)
 
 
 # xpath提供了一些节点轴的匹配方法，例如，获取祖节点、子孙节点、兄弟节点等。
+from lxml import etree
+
+html_str = '''
+<div class="video_scroll">
+<li> <a href="javascript:" onclick="login(0)" title="Java API文档">Java API文档</a> </li>
+<li> <a href="javascript:" onclick="login(0)" title="JDK的下载">JDK的下载</a> </li>
+<li> <a href="Javascript:" onclick="login(0)" title="JDK的安装">JDK的安装</a> </li>
+<li> <a href="javascript:" onclick="login(0)" title="配置JDK">配置JDK</a> </li>
+</div>
+'''
+html = etree.HTML(html_str)
+ancestors = html.xpath('//li[2]/ancestor::*')
+print('li[2]所有祖先节点名称：', [i.tag for i in ancestors])
+body = html.xpath('//li[2]/ancestor::body')
+print('li[2]指定祖先节点名称：', [i.tag for i in body])
+class_div = html.xpath('//li[2]/ancestor::*[@class="video_scroll"]')
+print('li[2]class="video_scroll"的祖先节点名称：', [i.tag for i in class_div])
+attributes = html.xpath('//li[2]/a/attribute::*')
+print('li[2]/a的所有属性值：', attributes)
+div_chlid = html.xpath('//div/child::*')
+print('div的所有子节点名称：', [i.tag for i in div_chlid])
+body_descendant = html.xpath('//body/descendant::*')
+print('body的所有子孙节点名称：', [i.tag for i in body_descendant])
+li_following = html.xpath('//li[1]/following::*')
+print('li[1]之后的所有节点名称：', [i.tag for i in li_following])
+li_sibling = html.xpath('//li[1]/following::*')
+print('li[1]之后的所有同级节点名称：', [i.tag for i in li_sibling])
+li_preceding = html.xpath('//li[3]/preceding::*')
+print('li[3]之前的所有节点名称：', [i.tag for i in li_preceding])
+
+结果：
+li[2]所有祖先节点名称： ['html', 'body', 'div']
+li[2]指定祖先节点名称： ['body']
+li[2]class="video_scroll"的祖先节点名称： ['div']
+li[2]/a的所有属性值： ['javascript:', 'login(0)', 'JDK的下载']
+div的所有子节点名称： ['li', 'li', 'li', 'li']
+body的所有子孙节点名称： ['div', 'li', 'a', 'li', 'a', 'li', 'a', 'li', 'a']
+li[1]之后的所有节点名称： ['li', 'a', 'li', 'a', 'li', 'a']
+li[1]之后的所有同级节点名称： ['li', 'a', 'li', 'a', 'li', 'a']
+li[3]之前的所有节点名称： ['li', 'a', 'li', 'a']
+
