9d1955d0eb86fec837bc27cc494689cd85238895
WilliamL71Oi <37209194+WilliamL71Oi@users.noreply.github.com>
2021-08-01 20:51:15 +0800

    Update 多线程进程.py

--- 爬虫/多线程进程.py
+++ 爬虫/多线程进程.py
@@ -220,5 +220,57 @@ if __name__ == '__main__':
     
     
     
+    # 使用Process子类创建进程
+    # 对于一些简单的小任务，通常使用Process(target=test)方式实现多进程。但是如果要处理复杂任务的进程，通常定义一个类，使其继承Process类，每次实例化一个类的时候，就等同于实例化一个进程对象。
+    # 使用Process子类创建多个进程
+    # 使用Process子类方式创建2个进程，分别输出父、子进程的PID，以及每个子进程的状态和运行时间。
+    
+    # -*- coding:utf-8 -*-
+
+from multiprocessing import Process
+import time
+import os
+
+
+# 继承Process类
+class SubProcess(Process):
+    # 由于Process类本身也有__init__初始化方法，这个子类相当于重写了父类的方法
+    def __init__(self, interval, name=""):
+        Process.__init__(self)  # 调用Process父类的初始化方法
+        self.interval = interval  # 接收参数interval
+        if name:
+            self.name = name  # 如果传递参数name，则为子进程创建name属性，否则使用默认属性
+
+    # 重写Process类的run()方法
+    def run(self):
+        print("子进程(%s开始执行，父进程为(%s)" % (os.getpid(), os.getpid()))
+        t_start = time.time()
+        time.sleep(self.interval)
+        t_stop = time.time()
+        print("子进程(%s)执行结束，耗时%0.2f秒" % (os.getpid(), t_stop - t_start))
+
+
+if __name__ == '__main__':
+    print("---父进程开始执行---")
+    print("父进程PID：%s" % os.getpid())
+    p1 = SubProcess(interval=1, name='mrsoft')
+    p2 = SubProcess(interval=2)
+    # 对一个不包含target属性的Process类执行start()方法，就会运行这个类中的run()方法
+    # 所以这里会执行p1.run()
+    p1.start()
+    p2.start()
+    # 输出p1和p2进程的执行状态，如果真正进行，返回True，否则返回False
+    print("p1.is_alive=%s"%p1.is_alive())
+    print("p2.is_alive=%s"%p2.is_alive())
+    # 输出p1和p2进程的别名和PID
+    print("p1.name=%s"%p1.name)
+    print("p1.pid=%s"%p1.pid)
+    print("p2.name=%s"%p2.name)
+    print("p2.pid=%s"%p2.pid)
+    print("---等待子进程---")
+    p2.join()  # 等待p1进程结束
+    p2.join()  # 等待p2进程结束
+    print("---父进程执行结束---")
+
     
     
