b025abd9a163415870d550194d097902e92136b0
WilliamL71Oi <37209194+WilliamL71Oi@users.noreply.github.com>
2021-08-09 18:35:40 +0800

    Update 多线程进程.py

--- 爬虫/多线程进程.py
+++ 爬虫/多线程进程.py
@@ -311,4 +311,41 @@ if __name__ == '__main__':
     # 先判断消息队列是否已满，不为满时再写入
     if not q.full():
         q.put_nowait("消息4")
+        
     
+
+# 创建2个子进程，一个子进程负责向队列中写入数据，另一个子进程负责从队列中读取数据。为保证能够正确从队列中读取数据，设置读取数据的进程等待时间为2秒。如果2秒后仍然无法读取数据，则抛出异常。    
+#! /usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+from multiprocessing import Process, Queue
+import time
+
+
+# 向队列中写入数据
+def write_task(q):
+    if not q.full():
+        for i in range(5):
+            message = "消息" + str(i)
+            q.put(message)
+            print("写入:%s" % message)
+
+
+# 从队列读取数据
+def read_task(q):
+    time.sleep(1)       # 休眠1秒
+    while not q.empty():        # 等待2秒，如果还没读取到任何消息
+        print("读取:%s" % q.get(True, 2))     # 则抛出"Queue.Empty"异常
+
+
+if __name__ == '__main__':
+    print("---父进程开始---")
+    q = Queue()     # 父进程创建Queue，并传给各个子进程
+    pw = Process(target=write_task, args=(q,))      # 实例化写入队列的子进程，并且传递队列
+    pr = Process(target=read_task, args=(q,))       # 实例化读取队列的子进程，并且传递队列
+    pw.start()      # 启动子进程pw，写入
+    pr.start()      # 启动子进程pr，读取
+    pw.join()       # 等待pw结束
+    pr.join()       # 等待pr结束
+    print("---父进程结束---")
+   
